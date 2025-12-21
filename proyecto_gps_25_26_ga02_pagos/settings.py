5cb863622b36e51460364d3f54593a115e4aa05d
dsanchezp25 <danisanchezp25@gmail.com>
2025-12-21 23:21:31 +0100

    UPDATE DB

--- proyecto_gps_25_26_ga02_pagos/settings.py
+++ proyecto_gps_25_26_ga02_pagos/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -83,24 +82,17 @@ WSGI_APPLICATION = 'proyecto_gps_25_26_ga02_pagos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'payments_db',
-         'USER': 'payments_db',
-         'PASSWORD': 'payments_password',
-         'HOST': 'db_pagos',
-         'PORT': 5432,
-     }
- }
-
+    'default': {
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DATABASE_NAME', BASE_DIR / 'db.sqlite3'),
+        # Los siguientes campos solo se usan si volviéramos a Postgres/MySQL
+        'USER': os.getenv('DATABASE_USER', ''),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
+        'HOST': os.getenv('DATABASE_HOST', ''),
+        'PORT': os.getenv('DATABASE_PORT', ''),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
